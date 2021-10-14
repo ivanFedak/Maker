116a82d3eaa15610759d3ed492cceac74b7870c3
Ivan Fedak <joing3497@gmail.com>
2021-10-14 18:42:16 +0300

    no Api

--- src/js/own/api.js
+++ src/js/own/api.js
@@ -1,11 +1,11 @@
-import getData from '../services/response'
-const api = ()=>{
+// import getData from '../services/response'
+// const api = ()=>{
 
 
-    const API_URL = `https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1`;
+//     const API_URL = ``;
 
-    getData(API_URL)
-        .then(res=>{console.log(res)}) 
+//     getData(API_URL)
+//         .then(res=>{console.log(res)}) 
 
-}
-export default api;
+// }
+// export default api;
--- src/js/services/response.js
+++ src/js/services/response.js
@@ -1,13 +1,8 @@
 const getData = async (url)=>{
 
-    const API_KEY = '8c8e1a50-6322-4135-8875-5d40a5420d86';
+    // const API_KEY = '8c8e1a50-6322-4135-8875-5d40a5420d86';
     
-    let res = await fetch(url, {
-        headers: {
-            "Content-Type": "application/json",
-            "X-API-KEY": API_KEY
-        },
-    });
+    let res = await fetch(url);
 
     if(!res.ok){
         throw new Error(`Could'n fetch ${url}, status${res.status}`)
